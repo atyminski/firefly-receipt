534d90379dd8e8017cf8381c6cd7bd36eecd0567
Arkadiusz Tymiński <tyminski.arek@hotmail.com>
2020-03-18 21:39:40 +0100

    nuke build

---
+++ build/Build.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath TestsDirectory => RootDirectory / "tests";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath ApplicationCsproj => SourceDirectory / "Gevlee.FireflyReceipt.Application" / "Gevlee.FireflyReceipt.Application.csproj";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore());
+        });
+
+    DotNetPublishSettings ApplyBasePublishSettings(DotNetPublishSettings settings)
+    {
+        settings.SetProject(ApplicationCsproj);
+        settings.SetConfiguration(Configuration);
+        settings.SetAssemblyVersion(GitVersion.AssemblySemVer);
+        settings.SetFileVersion(GitVersion.AssemblySemFileVer);
+        settings.SetInformationalVersion(GitVersion.InformationalVersion);
+        settings.EnableNoRestore();
+        return settings;
+    }
+
+    Target Publish => _ => _
+    .DependsOn(Restore)
+    .Executes(() =>
+    {
+
+        DotNetPublish(s => ApplyBasePublishSettings(s).SetRuntime("win-x64").SetOutput(ArtifactsDirectory / "win"));
+        DotNetPublish(s => ApplyBasePublishSettings(s).SetRuntime("linux-x64").SetOutput(ArtifactsDirectory / "linux"));
+    });
+
+}
